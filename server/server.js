67c7d7a40ffa8f9e732646ea5e9b3c91e58fe0a1
osycon <fredrichawk@gmail.com>
2017-09-30 18:25:11 +0200

    Add some comments

--- server/server.js
+++ server/server.js
@@ -7,6 +7,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(3000, () => {
-  // console.log('Express server ');
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+app.post('/', (req, res) => {
+  // Content from user comes in as req
+  const message = req.body.text;
+
+  const data = {
+    // Send message so it is only visible to the user.
+    response_type: 'ephemeral',
+    text: 'Working'
+  };
+  // Send message back to the user.
+  res.json(data);
 });
