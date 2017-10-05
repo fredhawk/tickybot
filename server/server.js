5f769c65ec4dcb0531cfb18d235812d75c2190cf
osycon <fredrichawk@gmail.com>
2017-10-05 07:18:01 +0200

    Refactored server to be more clean.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const firebaseDb = require('../config/firebase');
-const userHandler = require('./handlers/userHandler');
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,21 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+require('./api')(app);
+
 const server = app.listen(PORT, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
-
-app.post('/', (req, res) => {
-  // Content from user comes in as req
-  const { user_id, user_name, text } = req.body;
-  console.log(req.body);
-  // userHandler.writeUserData(user_id, user_name, text);
-  userHandler.addNewTicket(user_id, text);
-  const data = {
-    // Send message so it is only visible to the user.
-    response_type: 'ephemeral',
-    text: 'Working',
-  };
-  // Send message back to the user.
-  res.json(data);
 });
