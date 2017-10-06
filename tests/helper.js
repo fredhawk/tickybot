df85337024fb4f66a973af5499e38b47be51e9bd
osycon <fredrichawk@gmail.com>
2017-10-06 05:49:16 +0200

    Fixed lines

--- tests/helper.js
+++ tests/helper.js
@@ -1,1 +1,6 @@
-exports.users = [{ id: 1, username: 'lilly' }, { id: 2, username: 'nick' }, { id: 3, username: 'joe' }, { id: 4, username: 'julia' }];
+exports.users = [
+  { id: 1, username: 'lilly' },
+  { id: 2, username: 'nick' },
+  { id: 3, username: 'joe' },
+  { id: 4, username: 'julia' },
+];
