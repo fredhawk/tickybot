37b67852f4441af2b29c92218a74d27b390429a3
osycon <fredrichawk@gmail.com>
2017-10-06 05:53:07 +0200

    Fixed naming inconsistencies.

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,9 @@
 const firebase = require('firebase');
 // Initialize Firebase
 const config = {
-  apiKey: process.env.firebaseApiKey,
-  authDomain: process.env.firebaseAuthDomain,
-  databaseURL: process.env.firebaseDatabaseURL,
-  projectId: process.env.firebaseProjectId,
-  storageBucket: process.env.firebaseStorageBucket,
-  messagingSenderId: process.env.firebaseMessagingSenderId,
+  apiKey: process.env.FIREBASEAPIKEY,
+  databaseURL: process.env.FIREBASEDATABASEURL,
+  projectId: process.env.FIREBASEPROJECTID,
 };
 
 firebase.initializeApp(config);
