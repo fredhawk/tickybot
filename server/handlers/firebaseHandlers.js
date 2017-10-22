5d2757c1335da41f958ccf06cf505b73064a01d0
osycon <fredrichawk@gmail.com>
2017-10-22 15:15:49 +0200

    Refactored new ticket function. Made get all open ticket function. Made get all open ticket by user function

--- server/handlers/firebaseHandlers.js
+++ server/handlers/firebaseHandlers.js
@@ -1,11 +1,12 @@
 const firebase = require('firebase');
 require('../../config/firebase');
+const querybase = require('querybase');
 
 /**
  * Add User
- * 
- * @param {any} userId 
- * @param {any} username 
+ *
+ * @param {any} userId
+ * @param {any} username
  */
 exports.addUser = (userId, username) => {
   firebase
@@ -18,37 +19,97 @@ exports.addUser = (userId, username) => {
 
 /**
  * Add Ticket
- * 
- * @param {any} userId 
- * @param {any} text 
+ *
+ * @param {any} userId
+ * @param {any} text
  */
-exports.addNewTicket = async (userId, text) => {
+exports.addNewTicket = async (
+  userId,
+  teamId,
+  username,
+  text,
+  isAdmin,
+  status = 'open'
+) => {
   const tickets = firebase.database().ref('tickets/');
   const newTicket = tickets.push();
   newTicket.set({
     author: userId,
+    team: teamId,
+    username,
     text,
+    isAdmin,
+    status,
+    author_status: `${userID}_${status}`,
+    // ticketNumber,
   });
 };
 
 /**
  * Remove all ticket
  * 
- * @returns {Array of tickets}
  */
 exports.removeAllTickets = () => {
   const tickets = firebase.database().ref('tickets/');
   tickets.remove();
-  return tickets.on('child_removed', data => console.log(data));
 };
 
 /**
- * Get all tickets
+ * Get all open tickets
  * 
- * @returns {Array of tickets}
+ * @returns {collection of tickets}
  */
-exports.getAllTickets = async () => {
+exports.getAllOpenTickets = async () => {
   const tickets = firebase.database().ref('tickets/');
-  const values = await tickets.once('value');
+  const values = await tickets
+    .orderByChild('status')
+    .equalTo('open')
+    .once('value');
   return values.val();
+};
+
+/**
+ * Get all open tickets based on userId
+ * 
+ * @param {any} userId 
+ * @returns {collection of tickets}
+ */
+exports.getAllOpenTicketsByUser = async userId => {
+  const tickets = firebase.database().ref('tickets/');
+  const values = await tickets
+    .orderByChild('author_status')
+    .equalTo(`${userId}_open`)
+    .once('value');
+  return values.val();
+};
+
+exports.ticketCount = async () => {
+  const tickets = firebase.database().ref('tickets/');
+  tickets.on('child_added', snap => {
+    snap.count = snap.count + 1;
+    console.log(snap);
+  });
+  // const values = await tickets.once('value');
+  // const count = await va.numChildren();
+  // values.
+  console.log(count);
+  return count;
+};
+
+/**
+ * Get a specific ticket based on userId and ticketnumber
+ * 
+ * @param {any} userId 
+ * @param {any} ticketId 
+ * @returns {object}
+ */
+exports.getTicket = async (userId, ticketId) => {
+  const ticket = firebase.database().ref(`ticket/${ticketId}/`);
+  const value = await ticket.once('value');
+  // value
+  return value.val();
+};
+
+exports.updateTicket = async (userId, ticketId, ticketObj) => {
+  const ticket = firebase.database().ref(`ticket/${ticketId}/`);
 };
