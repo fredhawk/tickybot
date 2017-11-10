6f15e49debe76872b45d9a78533fa1dc67d0dfe0
Zsolt Meszaros <zsolt@sowad.com>
2017-11-10 22:29:00 +0000

    Add smooth scrolling

--- client/main.js
+++ client/main.js
@@ -1,1 +1,16 @@
+import jump from 'jump.js';
 import 'styles';
+
+document.querySelectorAll('a[href^="#"]')
+  .forEach((link) => {
+    link.addEventListener('click', (e) => {
+      jump(document.querySelector(e.target.hash), {
+        duration: 500,
+      });
+    });
+  });
+
+document.querySelector('.back-to-top')
+  .addEventListener('click', () => {
+    jump('body', { duration: 500 });
+  });
